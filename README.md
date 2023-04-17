692903231ff6957516b396b6e707e81f743aa00d
Théo RICHARD <mail@rcode.tech>
2023-04-17 20:11:15 +0200

    chore: template init

---
+++ README.md
@@ -0,0 +1,5 @@
+# <h1 align="center"> Foundryx Hardhat Template </h1>
+
+Contract to use for normal & VPR IGO but also any other extra cases where a project wants to allow people to buy tokens at a set price.
+
+More details on [Confluence](https://seedifymetastudios.atlassian.net/l/cp/bcc1Xhfr)
---
+++ test/foundry/IGO.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+import "../../src/IGO.sol";
+
+contract IGO_Test is Test {
+    IGO public instance;
+
+    function setUp() public {
+        instance = new IGO();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 BASIC ATTRIBUTES
+    //////////////////////////////////////////////////////////////*/
+    function test_truthy() public {
+        assertTrue(true);
+    }
+}
