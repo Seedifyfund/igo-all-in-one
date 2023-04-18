1cbd9d1dda40aa9f8810c08c28b981628c9671da
Théo RICHARD <mail@rcode.tech>
2023-04-18 21:07:49 +0200

    feat: define `Tag` structure

---
+++ src/writable/IIGOWritableInternal.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+interface IIGOWritableInternal {
+    enum State {
+        NOT_STARTED,
+        STARTED,
+        COMPLETED,
+        PAUSED
+    }
+
+    struct Tag {
+        State state;
+        // contains wallet and allocation per wallet
+        bytes32 merkleRoot;
+        uint128 startAt;
+        uint128 endAt;
+        uint256 maxTagCap;
+    }
+}
