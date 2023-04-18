b1b066ed18e4f8927aaa9c26df8fc584421a7b1e
Théo RICHARD <mail@rcode.tech>
2023-04-18 21:05:55 +0200

    chore: move to solidity `0.8.17`
    Due to forge unkown version with `0.8.18`

--- scripts/IGO.deploy.s.sol
+++ scripts/IGO.deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 import "../src/IGO.sol";
@@ -14,7 +14,7 @@ import "../src/IGO.sol";
 * forge verify-contract \
     --chain 97 \
     --num-of-optimizations 20000 \
-    --compiler-version v0.8.18+commit.87f61d96 \
+    --compiler-version v0.8.17+commit.87f61d96 \
     --watch 0xb7DEBdA47C1014763188E69fc823B973eC1749D6 \
     IGO $BSC_KEY
 *
