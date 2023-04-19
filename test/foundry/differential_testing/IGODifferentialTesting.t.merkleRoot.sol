c3ce6a061dac9f5ca1749c3ffb9b15439c22b35e
Théo RICHARD <mail@rcode.tech>
2023-04-19 03:11:50 +0200

    refactor: move differential tests into dedicated folder

--- test/foundry/differential_testing/IGODifferentialTesting.t.merkleRoot.sol
+++ test/foundry/differential_testing/IGODifferentialTesting.t.merkleRoot.sol
@@ -7,51 +7,8 @@ import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
 
 import "forge-std/Test.sol";
 
-import {IGO} from "../../src/IGO.sol";
-import {IIGOWritableInternal} from "../../src/writable/IIGOWritableInternal.sol";
-
-contract IGO_Test_merkleRoot is Test, IIGOWritableInternal {
-    IGO public instance;
-
-    uint256 public grandTotal = 50_000_000 ether;
-    string[] public tagIdentifiers;
-    Tag[] public tags;
-
-    function setUp() public {
-        instance = new IGO(grandTotal);
-
-        tagIdentifiers.push("vpr-base");
-        tagIdentifiers.push("vpr-premium1");
-        tagIdentifiers.push("vpr-premium2");
-        tagIdentifiers.push("igo-phase1");
-        tagIdentifiers.push("igo-phase2");
-        tagIdentifiers.push("igo-phase3");
-
-        uint128 lastStart = 60;
-        uint128 lastEnd = 1 hours;
-        uint256 maxTagAllocation = 1_000_000 ether;
-
-        for (uint256 i; i < tagIdentifiers.length; ++i) {
-            maxTagAllocation = 1_000_000 ether * (i + 1);
-
-            tags.push(
-                Tag(
-                    State.NOT_STARTED,
-                    bytes32(0),
-                    uint128(block.timestamp) + lastStart,
-                    uint128(block.timestamp) + lastEnd,
-                    maxTagAllocation
-                )
-            );
-
-            lastStart = lastEnd;
-            lastEnd += 1 hours;
-        }
-
-        instance.setTags(tagIdentifiers, tags);
-    }
-
-    function testFuzzDifferentail_generateLeaves_WithJSScript_CompareToSolidityLeaf(
+contract IGO_DifferentialTesting_merkleRoot is Test {
+    function testFuzzDifferential_generateLeaves_WithJSScript_CompareToSolidityLeaf(
         address[] memory addresses,
         uint256[] memory allocations
     ) public {
