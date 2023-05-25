70fd4edfa6fe1d46ad4b44b54bcf89decc0c0e7a
Théo RICHARD <mail@rcode.tech>
2023-05-25 16:46:55 +0200

    feat: add minimal proxy to deploy by clonning

--- src/IGOFactory.sol
+++ src/IGOFactory.sol
@@ -1,18 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import {Clones} from "openzeppelin-contracts/proxy/Clones.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
+import {IIGOWritable} from "./writable/IIGOWritable.sol";
 import {ISharedInternal} from "./shared/ISharedInternal.sol";
 
 import {IGO} from "./IGO.sol";
 
 /// @dev Contract to deploy IGOs one the fly, in one transaction
 contract IGOFactory is Ownable {
+    address public defaultIgo;
     string[] internal _igoNames;
-    mapping(string => IGO) internal _igos;
+    mapping(string => address) internal _igos;
 
+    event DefaultIgoUpdated(address oldDefaultIgo, address newDefaultIgo);
     event IGOCreated(string indexed igoName, address indexed igo);
+
+    constructor() {
+        defaultIgo = address(new IGO());
+    }
 
     function createIGO(
         string memory igoName,
@@ -22,14 +30,20 @@ contract IGOFactory is Ownable {
         uint256 grandTotal_,
         string[] memory tagIds_,
         ISharedInternal.Tag[] memory tags
-    ) external onlyOwner returns (IGO) {
+    ) external onlyOwner returns (address igo) {
         require(
             address(_igos[igoName]) == address(0),
             "IGOFactory: IGO already exists"
         );
 
-        IGO igo = new IGO();
-        igo.initialize(
+        // FIX: wallet is msg.sender
+        igo = Clones.cloneDeterministic(
+            defaultIgo,
+            keccak256(abi.encodePacked(_msgSender(), igoName))
+        );
+
+        // FIX: factory is msg.sender
+        IIGOWritable(igo).initialize(
             owner(),
             token,
             permit2,
@@ -43,11 +57,11 @@ contract IGOFactory is Ownable {
         _igos[igoName] = igo;
 
         emit IGOCreated(igoName, address(igo));
-
-        return igo;
     }
 
-    function igoWithName(string memory igoName) external view returns (IGO) {
+    function igoWithName(
+        string memory igoName
+    ) external view returns (address) {
         return _igos[igoName];
     }
 
@@ -57,5 +71,15 @@ contract IGOFactory is Ownable {
 
     function igoNames() external view returns (string[] memory) {
         return _igoNames;
+    }
+
+    function updateDefaultIgo(address newDefaultIgo) external onlyOwner {
+        require(
+            newDefaultIgo != address(0),
+            "IGOFactory__defaultIgo_ZERO_ADDRESS"
+        );
+        address oldDefaultIgo = defaultIgo;
+        defaultIgo = newDefaultIgo;
+        emit DefaultIgoUpdated(oldDefaultIgo, newDefaultIgo);
     }
 }
