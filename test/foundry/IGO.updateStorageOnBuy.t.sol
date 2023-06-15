b59cd90e03d04013ca1ff6e4f29f317a53946f04
Théo RICHARD <mail@rcode.tech>
2023-06-15 12:00:12 +0200

    fix(test): import

--- test/foundry/IGO.updateStorageOnBuy.t.sol
+++ test/foundry/IGO.updateStorageOnBuy.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import {IGOSetUp_require} from "./setUp/IGOSetUp_require.t.sol";
+import {IGOSetUp_require} from "./require/setUp/IGOSetUp_require.t.sol";
 
 contract IGO_Test_updateStorageOnBuy is IGOSetUp_require {
     function test_updateStorageOnBuy_CloseTagWhenEndDateReached() public {
